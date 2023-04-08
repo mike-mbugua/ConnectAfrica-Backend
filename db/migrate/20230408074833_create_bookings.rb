923e1cafddea44cfc7cd1cbbc5614b0cfc3845be
michael-mbugua <kamirimichael369@gmail.com>
2023-04-08 21:12:43 +0300

    create table columns

--- db/migrate/20230408074833_create_bookings.rb
+++ db/migrate/20230408074833_create_bookings.rb
@@ -1,6 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
+      t.string :fullName
+      t.integer :phone
+      t.date :date
+      t.integer :guests
 
       t.timestamps
     end
