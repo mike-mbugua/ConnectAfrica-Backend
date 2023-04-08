9fc27f8adc4aa7cdb4f42f00829ee5e161ccad43
michael-mbugua <kamirimichael369@gmail.com>
2023-04-08 21:10:57 +0300

    add controller action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,21 @@
 class ContactsController < ApplicationController
+    def index
+        contact=contact.all
+        render json:contact ,status: :ok
+    end
+
+    def create
+        contact=Contact.create(contact_params)
+        render json: contact, status: :ok
+    end
+
+    def destroy
+        contact=contact.find()
+        contact.destroy
+    end
+
+    private
+    def contact_params
+        params.permit(:email,:message)
+    end
 end
