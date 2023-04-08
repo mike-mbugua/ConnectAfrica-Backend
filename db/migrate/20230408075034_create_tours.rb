c077238f8b7fd5349ee928d818c290f093870439
michael-mbugua <kamirimichael369@gmail.com>
2023-04-08 21:13:11 +0300

    create table columns

--- db/migrate/20230408075034_create_tours.rb
+++ db/migrate/20230408075034_create_tours.rb
@@ -1,6 +1,16 @@
 class CreateTours < ActiveRecord::Migration[7.0]
   def change
     create_table :tours do |t|
+      t.string :image
+      t.string :title
+      t.string :city
+      t.integer :distance
+      t.integer :price
+      t.integer :maxGroupSize
+      t.text :description
+      t.decimal :rating
+      t.boolean :featured
+      
 
       t.timestamps
     end
