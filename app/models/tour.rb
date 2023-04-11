cd3a534bc4c00dcc4e3bac99aef86cdaa03e6d10
michael-mbugua <kamirimichael369@gmail.com>
2023-04-11 12:33:43 +0300

    add search methods and parameters to search from

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,14 @@
 class Tour < ApplicationRecord
     validates :image, :title,:city,:distance,:price,:maxGroupSize,:description,:rating,:featured, presence: true
+
+    pg_search_scope :search_by_title_and_city,
+    against: %i[title  city price maxGroupSize featured],
+    using: {
+        tsearch: {
+            prefix: true
+        }
+    }
+
+
+
 end
